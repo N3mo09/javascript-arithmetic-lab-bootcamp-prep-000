6d84caa61e5b2d15be1680c2a2a3a1318ef86e0d
N3mo09 <zach.spoor@gmail.com>
2017-10-09 18:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ function makeInt(a) {
   return parseInt('a', 10)
 }
 
+function preserveDecimal(2.222) {
+  return preserveDecimal(2.222)
+}
+
 function preserveDecimal(a) {
   return parseFloat('a')
 }
